c2e6dce1177558f911c44a8f2a9fef7c89cdcb04
partykaj <joanna.partyka@comarch.com>
2022-02-06 22:34:07 +0100

    Fixed merkle tree tests

--- tests/test_merkle_tree.py
+++ tests/test_merkle_tree.py
@@ -1,34 +1,40 @@
+import json
 import unittest
+
 from utils import compute_hash
 from merkle_tree import get_merkle_root
 
-transactions = ["A", "B"]
-transaction = ["A"]
-three_transactions = ["A", "B", "C"]
+TRANSACTION_DATA_1 = json.dumps({"Sender": "Address1", "Recipient": "Address2", "Amount": 200})
+TRANSACTION_DATA_2 = json.dumps({"Sender": "Address1", "Recipient": "Address3", "Amount": 100})
+TRANSACTION_DATA_3 = json.dumps({"Sender": "Address2", "Recipient": "Address3", "Amount": 50})
+
+TRANSACTIONS_DATA_1 = [TRANSACTION_DATA_1]
+TRANSACTIONS_DATA_2 = [TRANSACTION_DATA_1, TRANSACTION_DATA_2]
+TRANSACTIONS_DATA_3 = [TRANSACTION_DATA_1, TRANSACTION_DATA_2, TRANSACTION_DATA_3]
 
 
 class TestMerkleTree(unittest.TestCase):
+    def test_merkle_root_when_one_transaction(self):
+        leaf = compute_hash(TRANSACTIONS_DATA_1[0])
+        self.assertEqual(leaf, get_merkle_root(TRANSACTIONS_DATA_1))
+
     def test_merkle_root_when_two_leaves(self):
-        left_leaf = compute_hash(transactions[0])
-        right_leaf = compute_hash(transactions[1])
+        left_leaf = compute_hash(TRANSACTIONS_DATA_2[0])
+        right_leaf = compute_hash(TRANSACTIONS_DATA_2[1])
         expected_merkle_root = compute_hash(left_leaf + right_leaf)
-        merkle_root = get_merkle_root(transactions)
+        merkle_root = get_merkle_root(TRANSACTIONS_DATA_2)
         self.assertEqual(expected_merkle_root, merkle_root)
 
-    def test_merkle_root_when_one_transaction(self):
-        leaf = compute_hash(transaction[0])
-        self.assertEqual(leaf, get_merkle_root(transaction))
-
     def test_merkle_root_when_three_transactions(self):
-        leaf_0 = compute_hash(three_transactions[0])
-        leaf_1 = compute_hash(three_transactions[1])
-        leaf_2 = compute_hash(three_transactions[2])
-        leaf_3 = compute_hash(three_transactions[2])
+        leaf_0 = compute_hash(TRANSACTIONS_DATA_3[0])
+        leaf_1 = compute_hash(TRANSACTIONS_DATA_3[1])
+        leaf_2 = compute_hash(TRANSACTIONS_DATA_3[2])
+        leaf_3 = compute_hash(TRANSACTIONS_DATA_3[2])
 
         parent_1 = compute_hash(leaf_0 + leaf_1)
         parent_2 = compute_hash(leaf_2 + leaf_3)
 
         expected_merkle_root = compute_hash(parent_1 + parent_2)
-        merkle_root = get_merkle_root(three_transactions)
+        merkle_root = get_merkle_root(TRANSACTIONS_DATA_3)
 
         self.assertEqual(expected_merkle_root, merkle_root)
